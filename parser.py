ce44dcffc8e09fc8b48bf138bae90192f90bacdc
ehsanmqn <spin313@gmail.com>
2020-04-04 19:55:01 +0430

    Merging data performed with duplication handling

--- parser.py
+++ parser.py
@@ -2,38 +2,81 @@ import os
 import json
 from html2json import collect
 
-regex = {
+regex2015 = {
     "artist": ["h2", "", ""],
     "work": ["h3", "", ""],
     "price": ["div", "", ["/[GU][BS][PD].*/"]]
 }
 
-def to_json(x):
+
+regex2017 = {
+    "h3": ["h3", "", ""],
+    "currency": ["span", "", ["/[GU][BS][PD].*/"]],
+    "amount": ["span", "", ["/[0-9].*/"]]
+}
+
+
+def to_json_2017(x):
+    return {
+        "artist": x["h3"].split("|")[0],
+        "works": [{
+            "title": x["h3"].split("|")[1],
+            "currency": x["currency"].split("|")[0],
+            "totalLifetimeValue": x["amount"]
+        }]
+    }
+
+
+def to_json_2015(x):
     return {
         "artist": x["artist"],
         "works": [{
             "title": x["work"].split("|")[0],
             "currency": x["price"].split(" ")[0],
-            "amount": x["price"].split(" ")[1]
+            "totalLifetimeValue": x["price"].split(" ")[1]
         }]
     }
 
-def merge_duplicates(dict1, dict2):
-    for key, value in dict1.items():
-        if key in dict2:
-            dict1[key]["works"].append(dict2["works"])
+
+def add_to_dict_without_duplication(dict, json_data):
+    for key, value in dict.items():
+        if key == json_data["artist"]:
+            dict[key]["works"].append(json_data["works"])
+            return dict
+
+    dict[json_data["artist"]] = json_data
+
+    return dict
 
 
-def parse_html_data_2015():
+def parse_html_data_2015(data=None):
     working_directory = "data/2015-03-18/"
     entries = os.listdir(working_directory)
 
-    data = {}
+    if data == None:
+        data = {}
+
     for entry in entries:
         html_content = open(working_directory + entry, "r")
 
-        parsed = collect(html_content.read(), regex)
-        data[parsed["artist"]] = to_json(parsed)
+        parsed = collect(html_content.read(), regex2015)
+        add_to_dict_without_duplication(data, to_json_2015(parsed))
+
+    return data
+
+
+def parse_html_data_2017(data=None):
+    working_directory = "data/2017-12-20/"
+    entries = os.listdir(working_directory)
+
+    if data == None:
+        data = {}
+
+    for entry in entries:
+        html_content = open(working_directory + entry, "r")
+
+        parsed = collect(html_content.read(), regex2017)
+        add_to_dict_without_duplication(data, to_json_2017(parsed))
 
     return data
 
@@ -48,4 +91,8 @@ if __name__ == '__main__':
     # print(list(map(lambda x: to_json(x), parsed_data)))
 
     # Step 4
+    # print(list(dict.values(parsed_data)))
+
+    # Step 5
+    parsed_data = parse_html_data_2017(parsed_data)
     print(list(dict.values(parsed_data)))
