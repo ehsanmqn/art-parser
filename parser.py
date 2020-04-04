f3c22d64d9aaef43e75ac16e658471fabb81acae
ehsanmqn <spin313@gmail.com>
2020-04-04 17:56:04 +0430

    Removing duplications added

--- parser.py
+++ parser.py
@@ -1,4 +1,5 @@
 import os
+import json
 from html2json import collect
 
 regex = {
@@ -10,23 +11,29 @@ regex = {
 def to_json(x):
     return {
         "artist": x["artist"],
-        "works": {
+        "works": [{
             "title": x["work"].split("|")[0],
             "currency": x["price"].split(" ")[0],
             "amount": x["price"].split(" ")[1]
-        }
+        }]
     }
+
+def merge_duplicates(dict1, dict2):
+    for key, value in dict1.items():
+        if key in dict2:
+            dict1[key]["works"].append(dict2["works"])
+
 
 def parse_html_data_2015():
     working_directory = "data/2015-03-18/"
     entries = os.listdir(working_directory)
 
-    data = []
+    data = {}
     for entry in entries:
         html_content = open(working_directory + entry, "r")
 
         parsed = collect(html_content.read(), regex)
-        data.append(parsed)
+        data[parsed["artist"]] = to_json(parsed)
 
     return data
 
@@ -38,4 +45,7 @@ if __name__ == '__main__':
     # print(list(dict.fromkeys(parsed_data)))
 
     # Step 2 to 3
-    print(list(map(lambda x: to_json(x), parsed_data)))
+    # print(list(map(lambda x: to_json(x), parsed_data)))
+
+    # Step 4
+    print(list(dict.values(parsed_data)))
