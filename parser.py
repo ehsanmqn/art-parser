a559070087e0044e66eb720cd39148005a0486ec
ehsanmqn <spin313@gmail.com>
2020-04-04 14:49:08 +0430

    Modify your script to extract the title of the work has done

--- parser.py
+++ parser.py
@@ -3,8 +3,14 @@ from html2json import collect
 
 regex = {
     "artist": ["h2", "", ""],
+    "work": ["h3", "", ""],
 }
 
+def to_json(x):
+    return {
+        "artist": x["artist"],
+        "works": x["work"].split("|")[0],
+    }
 
 def parse_html_data_2015():
     working_directory = "data/2015-03-18/"
@@ -15,11 +21,16 @@ def parse_html_data_2015():
         html_content = open(working_directory + entry, "r")
 
         parsed = collect(html_content.read(), regex)
-        data.append(parsed["artist"])
+        data.append(parsed)
 
     return data
 
 
 if __name__ == '__main__':
     parsed_data = parse_html_data_2015()
-    print(list(dict.fromkeys(parsed_data)))
+
+    # Step 1
+    # print(list(dict.fromkeys(parsed_data)))
+
+    # Step 2
+    print(list(map(lambda x: to_json(x), parsed_data)))
